59c2aceaa1afa2dbb13851467d20dd8761544578
Karan Yadav <ksyadav01@outlook.com>
2021-08-31 18:05:00 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "Personal Website V2",
+  },
+  plugins: [
+    "gatsby-plugin-netlify-cms",
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "UA-189239857-1",
+      },
+    },
+  ],
+};
